dc94ddd246c813fd231f5fdf1ed9644f6e194a7b
Olga <olchiknega@mail.ru>
2022-09-05 19:53:25 +0700

    in process

--- 64-66-68/Program.cs
+++ 64-66-68/Program.cs
@@ -13,9 +13,9 @@ namespace _64_66_68
                 case 64:
                     TaskSixtyfour();
                     break;
-                case 66:
+              /*  case 66:
                     TaskSixtysix();
-                    break;
+                    break;*/
                 case 58:
                     TaskSixtyeight();
                     break;
@@ -31,25 +31,53 @@ namespace _64_66_68
         N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
         static void TaskSixtyfour()
         {
-            Console.WriteLine("Введите N: ");
-            int N = Convert.ToInt32(Console.ReadLine());
-             
-            void PrintNumbersFromNtoOne(int N, int n = 1)
+            try
             {
-                if (n > N) return;
-                PrintNumbersFromNtoOne(N, n++);
-                Console.Write(n + " ");
+                Console.WriteLine("Введите N: ");
+                int N = Convert.ToInt32(Console.ReadLine());
+                PrintNumbersFromNtoOne(N);
             }
-            PrintNumbersFromNtoOne(N);
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
-
+        static void PrintNumbersFromNtoOne(int N, int n = 1)
+        {
+            if (n > N) return;
+            PrintNumbersFromNtoOne(N, n+1);
+            Console.Write(n + " ");
+        }
         #endregion
 
         #region Задача 66
+        /*Задайте значения M и N.
+        Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+        M = 1; N = 15 -> 120
+        M = 4; N = 8. -> 30*//*
         static void TaskSixtysix()
         {
+            Console.WriteLine("Введите M: ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            *//*int sum = 0;*//*
+            if (M < N)
+            {
+                void PrintSumNumbersFromMtoN(int N, int M)
+                {
+                    if (N < M) return;
+                    PrintSumNumbersFromMtoN(N - 1, M);
+                    Console.Write(N + " ");
+                }
+                PrintSumNumbersFromMtoN(N, M);
+            }
+            else Console.WriteLine("Первое число M должно быть меньше второго числа N. Введите заново.");
+            
+        }*/
 
-        }
+
 
         #endregion
 
